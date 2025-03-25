6e7e1e86ac00081999b21bacb3395f5464b6552e
annie <annemarie.ala@edu.sait.ca>
2025-03-24 22:51:56 -0400

    Update item.js

--- app/week-8/item.js
+++ app/week-8/item.js
@@ -1,6 +1,6 @@
 export default function Item({ name, quantity, category, onSelect }) {
     return (
-      <section className="bg-gray-900 p-4 m-4 rounded-lg" onClick={() => onSelect(item)}>
+      <section className="bg-gray-900 p-4 m-4 rounded-lg" onClick={() => onSelect(name, quantity, category)}>
 
         <p className="font-bold space-x-2 text-gray-100">{name}</p>
         <p className="font-thin text-gray-100">Buy  {quantity} in {category}</p>
